b48c921bc41f42ba48e343da7414316d18e0fced
Pablo David Gonzalez <93014456+Pablog95@users.noreply.github.com>
2022-05-23 22:37:17 -0300

    Update nodo.h

--- nodo.h
+++ nodo.h
@@ -19,4 +19,22 @@ class Nodo
 		T obtenerDato();
 };
 
+template <class T>
+Nodo<T> :: Nodo(T _dato){
+	dato = _dato;
+	siguiente = 0;
+}
+template <class T>
+void Nodo<T> :: cambiarSiguiente (Nodo* _siguiente){
+	siguiente = _siguiente;
+}
+template <class T>
+Nodo<T>* Nodo<T> :: obtenerSiguiente(){
+	return siguiente;
+}
+template <class T>
+T Nodo<T>::obtenerDato(){
+	return dato;
+}
+
 #endif
