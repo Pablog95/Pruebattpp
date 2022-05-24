6680f75625b013afa7afd4a8ff761f9b83084dfb
Pablo David Gonzalez <93014456+Pablog95@users.noreply.github.com>
2022-05-24 02:20:30 -0300

    Update historica.cpp

--- historica.cpp
+++ historica.cpp
@@ -1,6 +1,6 @@
 #include "historica.h"
 
-Historica :: Historica (string _titulo, unsigned int _minutos, int _anio, string _autor,string _genero, char _tema):
+Historica :: Historica (string _titulo, string _minutos, string _anio, string _autor,string _genero, char _tema):
 									Novela (titulo,minutos,anio,autor,genero)
 {
 	this->tema = _tema;
