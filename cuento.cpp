9c48135ae4de26b61eefc134f9280aa696a3dd4c
Pablo David Gonzalez <93014456+Pablog95@users.noreply.github.com>
2022-05-24 02:19:25 -0300

    Update cuento.cpp

--- cuento.cpp
+++ cuento.cpp
@@ -1,7 +1,7 @@
 #include "cuento.h"
 
-Cuento :: Cuento(string _titulo, unsigned int _minutos, int _anio, string _autor, string _tituloLibro) :
-																	Lectura (titulo,minutos,anio,autor)
+Cuento :: Cuento(string _titulo, string _minutos, string _anio, string _autor, string _tituloLibro) :
+												Lectura (titulo,minutos,anio,autor)
 																	
 {
 	this->tituloLibro = _tituloLibro;
