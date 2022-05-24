d1753866be2aa9b34dcedd272e01947a2f0c5174
Pablo David Gonzalez <93014456+Pablog95@users.noreply.github.com>
2022-05-23 22:37:55 -0300

    Update poema.h

--- poema.h
+++ poema.h
@@ -2,7 +2,7 @@
 #define POEMA_H
 #include "lectura.h"
 
-class Poema
+class Poema : public Lectura
 {
 	public:
 		Poema(string _titulo, string _minutos, string _anio,string _autor, string _cantidadVersos);
