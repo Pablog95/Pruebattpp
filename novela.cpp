9b91730906b4c9dfecc76d7024acef0ff8a7bc89
Pablo David Gonzalez <93014456+Pablog95@users.noreply.github.com>
2022-05-24 02:20:45 -0300

    Update novela.cpp

--- novela.cpp
+++ novela.cpp
@@ -1,6 +1,6 @@
 #include "novela.h"
 
-Novela :: Novela(string _titulo, unsigned int _minutos, int _anio, string _autor, string _genero):
+Novela :: Novela(string _titulo, string _minutos, string _anio, string _autor, string _genero):
 											Lectura(titulo,minutos,anio,autor)
 {
 	this->genero = _genero;
