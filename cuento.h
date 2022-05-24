6c78b128897bcddd545923521a47a9e7a85bd776
Pablo David Gonzalez <93014456+Pablog95@users.noreply.github.com>
2022-05-24 02:22:32 -0300

    Update cuento.h

--- cuento.h
+++ cuento.h
@@ -8,7 +8,7 @@ using namespace std;
 class Cuento : public Lectura
 {
 	public:
-		Cuento(string _titulo, unsigned int _minutos, int _anio, string _autor, string _tituloLibro);
+		Cuento(string _titulo, string _minutos, string _anio, string _autor, string _tituloLibro);
 	private:
 		string tituloLibro;
 		
