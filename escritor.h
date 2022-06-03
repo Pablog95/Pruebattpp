98558235830aa57cbcfc9f1ea72f2c52d7fd968c
Pablog95 <93014456+Pablog95@users.noreply.github.com>
2022-06-03 20:14:01 -0300

    Actualizo escritor.
    Agrego pre y post

--- escritor.h
+++ escritor.h
@@ -15,11 +15,30 @@ class Escritor
         int anioFallecimiento;
 
 	public:
+		//Constructor.
+		//PRE: -
+		//POST: Inicializa Escritor con sus atributos.
 		Escritor(string referencia,string _nombreApellido, string _nacionalidad, int _anioNacimiento, int _anioFallecimiento);
+
+		//PRE: -
+		//POST: Muestra por pantalla los atributos.
 		void mostrarDatos();
+		
+		//PRE: - 
+		//POST: Devuelve nombre y apellido.
 		string obtenerNombreApellido();
+
+		//PRE: Recibe anio valido.
+		//POST: Cambia el anio de Fallecimiento.
 		void cambiarFallecimiento();
-        string obtenerReferencia();
+	
+		//PRE:-
+		//POST: Devuelve la referencia.
+        	string obtenerReferencia();
+	
+		//Destructor.
+		//PRE: -
+		//POST: Libera los recursos.
 		~Escritor(){};
 
 };
